678358e3698991cf6bb483a2c5594b9f79714958
Moonbbyte <63923585+Moonbbyte@users.noreply.github.com>
2020-09-10 11:50:51 -0600

    Menusterminados

--- Main.py
+++ Main.py
@@ -1,4 +1,13 @@
 import Menus
 
 Menus.Bienvenida.impbienvenida("")
-print("XD")
+Menus.Bienvenida.Menuprincipal("")
+n=int(input("Ingrese una opcion: "))
+if n>4 or n<1:
+    print("\n------------------Opcion incorrecta--------------------")
+else:
+    while n!=4:
+        if n==1:
+            Menus.Bienvenida.Menuafd("")
+        elif n==2:
+            Menus.Bienvenida.Menugramaticas("")
--- Menus.py
+++ Menus.py
@@ -11,3 +11,17 @@ class Bienvenida:
         print("2.Modulo de gramaticas regulares: ")
         print("3.Acerca de: ")
         print("4.Salir")
+    def Menuafd(self):
+        print("1.Crear AFD")
+        print("2.Cargar Archivo")
+        print('3.Evaluar cadena')
+        print("4.Guardar AFD en archivo")
+        print("5.Generar reporte AFD")
+        print("6.Generar Gramatica Regular")
+    def Menugramaticas(self):
+        print("1.Crear Gramaticas")
+        print("2.Cargar archivo de entrada")
+        print('3.Evaluar cadena')
+        print("4.Eliminar recursividad por la izquierda")
+        print("5.Generar AFD en archivo")
+        print("6.Generar reporte de Gramatica Regular")
