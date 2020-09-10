3ab6c9f3a656ac9dc99685042fa25b8be7ee79a5
Moonbbyte <63923585+Moonbbyte@users.noreply.github.com>
2020-09-10 11:33:12 -0600

    Primeros-menus

---
+++ Main.py
@@ -0,0 +1,4 @@
+import Menus
+
+Menus.Bienvenida.impbienvenida("")
+print("XD")
---
+++ Menus.py
@@ -0,0 +1,13 @@
+
+class Bienvenida:
+    def impbienvenida(self):
+        print("Lenguajes Formales y de Programacion ")
+        print("Sección: B")
+        print("Carnet: 201800534")
+        input("Presiones una tecla para continuar...")
+    def Menuprincipal(self):
+        print("\n---------------Menu principal----------------")
+        print("1.Modulo AFD:")
+        print("2.Modulo de gramaticas regulares: ")
+        print("3.Acerca de: ")
+        print("4.Salir")
